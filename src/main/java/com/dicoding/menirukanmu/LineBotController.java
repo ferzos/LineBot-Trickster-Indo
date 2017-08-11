cf7fe70869083870dbadbdaeb4d41b3b3cba462f
Ferdinand <ferdifenrir@gmail.com>
2017-08-11 16:52:44 +0700

    fixing jumlah tepokan

--- src/main/java/com/dicoding/menirukanmu/LineBotController.java
+++ src/main/java/com/dicoding/menirukanmu/LineBotController.java
@@ -278,9 +278,9 @@ public class LineBotController
                         } else if (Integer.parseInt(arrInput[1]) == 3) {
                             variables.put("flagSoal",3);
                             int soalNumber = (int) (Math.random() * (7 - 0));
-                            int jumlahTepokan = (int) (Math.random() * (10 - 2));
-                            String plok = "";
-                            for (int i = 0; i < jumlahTepokan; i++) {
+                            int jumlahTepokan = (int) (Math.random() * 10);
+                            String plok = "Plok!! ";
+                            for (int i = 0; i < jumlahTepokan ; i++) {
                                 plok += "Plok!! ";
                             }
                             String soalBundle = headerMessage + plok + "\n" + soal3.get(soalNumber) + " ?\n\n" + endMessage;
